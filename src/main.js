45ddfa54c4fd8b620cc957f6f26c87db7a334f21
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2025-03-11 11:51:15 -0700

    Create Vite & Vue app Add Bootstrap with Vue & Vite integration Add Eslint with rules

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import * as bootstrap from "bootstrap";
+
+import App from "./App.vue";
+
+// Import our custom CSS
+import "./scss/styles.scss";
+import "./style.css";
+
+
+
+createApp(App).mount("#app");
