77376ead369c80b88a1136d8bc2261c0b1ba4f36
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2025-03-20 17:46:12 -0700

    Add style.css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from "./router.js";
 // App & CSS
 import App from "./App.vue";
 import "./scss/styles.scss";
+import "./style.css";
 
 // Components
 import NavBar from "./components/NavBar.vue";
